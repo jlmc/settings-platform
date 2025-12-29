c0b6fd34d47442ff121a88a9483a878104daa477
Joao Costa <costajlmpp@gmail.com>
2025-12-29 23:53:05 +0000

    feat: enhance Redis shared cache integration with configuration support and health check updates

--- poc-tdk-settings-l1-l2-service/src/main/java/io/github/jlmc/poc/tdk_settings_l1_l2/service/adapters/sharedcache/SharedCacheConfiguration.java
+++ poc-tdk-settings-l1-l2-service/src/main/java/io/github/jlmc/poc/tdk_settings_l1_l2/service/adapters/sharedcache/SharedCacheConfiguration.java
@@ -3,22 +3,48 @@ package io.github.jlmc.poc.tdk_settings_l1_l2.service.adapters.sharedcache;
 import io.github.jlmc.poc.tdk_settings_l1_l2.service.adapters.sharedcache.noop.NoOpSharedCacheSynchronizer;
 import io.github.jlmc.poc.tdk_settings_l1_l2.service.adapters.sharedcache.redis.RedisSharedCacheSynchronizer;
 import io.github.jlmc.poc.tdk_settings_l1_l2.service.domain.ports.SharedCacheSynchronizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableConfigurationProperties(SharedCacheConfigurationProperties.class)
 public class SharedCacheConfiguration {
 
     @Bean
-    public SharedCacheSynchronizer sharedCacheSynchronizer(SharedCacheConfigurationProperties properties) {
-        return switch (properties.type()) {
-            case NOOP -> new NoOpSharedCacheSynchronizer();
-            case REDIS -> new RedisSharedCacheSynchronizer();
-            case null, default ->
-                    throw new IllegalStateException("Unsupported SharedCacheSynchronizer type: " + properties.type());
-        };
+    @ConditionalOnProperty(name = "sharedcache.type", havingValue = "NOOP", matchIfMissing = true)
+    public SharedCacheSynchronizer noOpSharedCacheSynchronizer() {
+        return new NoOpSharedCacheSynchronizer();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "sharedcache.type", havingValue = "REDIS")
+    public SharedCacheSynchronizer redisSharedCacheSynchronizer(RedisTemplate<String, Object> redisTemplate) {
+        return new RedisSharedCacheSynchronizer(redisTemplate);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "sharedcache.type", havingValue = "REDIS")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        GenericJacksonJsonRedisSerializer jacksonJsonRedisSerializer = new GenericJacksonJsonRedisSerializer(objectMapper);
+        template.setValueSerializer(jacksonJsonRedisSerializer);
+
+        template.setHashValueSerializer(jacksonJsonRedisSerializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
     }
 
 }
--- poc-tdk-settings-l1-l2-service/src/main/java/io/github/jlmc/poc/tdk_settings_l1_l2/service/adapters/sharedcache/redis/RedisSharedCacheSynchronizer.java
+++ poc-tdk-settings-l1-l2-service/src/main/java/io/github/jlmc/poc/tdk_settings_l1_l2/service/adapters/sharedcache/redis/RedisSharedCacheSynchronizer.java
@@ -1,11 +1,20 @@
 package io.github.jlmc.poc.tdk_settings_l1_l2.service.adapters.sharedcache.redis;
 
+import io.github.jlmc.poc.tdk_settings_l1_l2.service.domain.entities.ConfigurationType;
 import io.github.jlmc.poc.tdk_settings_l1_l2.service.domain.entities.SettingsAccount;
 import io.github.jlmc.poc.tdk_settings_l1_l2.service.domain.ports.SharedCacheSynchronizer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Slf4j
 public class RedisSharedCacheSynchronizer implements SharedCacheSynchronizer {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisSharedCacheSynchronizer(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public void update(SettingsAccount settingsAccount) {
       log.debug("updating shared cache with SettingsAccount for accountId='{}', type='{}', serviceName='{}' - REDIS",
@@ -14,11 +23,18 @@ public class RedisSharedCacheSynchronizer implements SharedCacheSynchronizer {
 
     @Override
     public void delete(SettingsAccount settingsAccount) {
-        //TODO: implement Redis delete logic
+        log.debug("deleting SettingsAccount from shared cache for accountId='{}', type='{}', serviceName='{}' - REDIS",
+                settingsAccount.accountId(), settingsAccount.type(), settingsAccount.serviceName());
+        redisTemplate.delete(key(settingsAccount.accountId(), settingsAccount.serviceName(), settingsAccount.type()));
     }
 
     @Override
     public void hit(HitData hitData) {
+        redisTemplate.opsForValue()
+                .set(key(hitData.accountId(), hitData.serviceName(), hitData.configurationType()), hitData);
+    }
 
+    private String key(String accountId, String serviceName, ConfigurationType configurationType) {
+        return "settings-account:" + accountId + ":" + ":" + serviceName + ":" + configurationType.name();
     }
 }
