6ffb709757e076ab2232f2aa7286627c45923a29
Joao Costa <costajlmpp@gmail.com>
2025-12-30 03:49:37 +0000

    feat: initialize poc-tdk-settings-l1-l2-app-web-flux module with Spring Boot application and Maven wrapper

---
+++ poc-tdk-l1-l2-app-web-flux/src/test/java/io/github/jlmc/poc/tdk/settings/l1l2/appwebflux/PocTdkSettingsL1L2AppWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jlmc.poc.tdk.settings.l1l2.appwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocTdkSettingsL1L2AppWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
